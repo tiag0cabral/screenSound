b100ab8d5390138dae2460bf6f0684eb3ebe04cb
Tiago Cabral <tiagocabraldefaria@gmail.com>
2026-01-31 16:19:41 -0300

    Isolando o código com funções

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,12 @@
-﻿string identificacaoDoSistema = "Screen Sound";
-Console.WriteLine(identificacaoDoSistema);
+﻿// Nome de variáveis em camelCase
+string mensagemDeBoasVindas = "* Bem-vindo ao ScreenSound *";
+
+// Nome de funções em PascalCase
+void MensagemDeBoasVindas()
+{
+    Console.WriteLine("****************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("****************************");
+}
+
+MensagemDeBoasVindas();
